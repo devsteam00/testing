e69752daf58bb6e2d5bb74b78438e33c13df006f
Raghul <133097101+raghul292001@users.noreply.github.com>
2024-12-19 15:18:51 +0530

    Update sort.py

--- sort.py
+++ sort.py
@@ -17,7 +17,14 @@ X = [[12,7],
     [4 ,5],
     [3 ,8]]
 result = [[X[j][i] for j in range(len(X))] for i in range(len(X[0]))]
+old_list = [[1, 2, 3], [4, 5, 6], [7, 8, 'a']]
+new_list = old_list
 
-for r in result:
-   print(r)
+new_list[2][2] = 9
+
+print('Old List:', old_list)
+print('ID of Old List:', id(old_list))
+
+print('New List:', new_list)
+print('ID of New List:', id(new_list))
 
