ec612ad2b5ea82c0d6d8a36cbfec9ea88c7bd6a8
Raghul <133097101+raghul292001@users.noreply.github.com>
2024-12-19 14:42:13 +0530

    Update sort.py

--- sort.py
+++ sort.py
@@ -2,4 +2,5 @@ wrdstr = "Hello this Is an Example With cased letters"
 words = [word.lower() for word in wrdstr.split()]
 words.sort()
 print("The sorted words are:")
-
+for word in words:
+   print(word)
