c0d9fd02e1d1cb113571fdedfad4cf0d9730ca1d
Raghul <133097101+raghul292001@users.noreply.github.com>
2024-12-19 15:10:31 +0530

    Update sort.py

--- sort.py
+++ sort.py
@@ -13,7 +13,11 @@ result = [[0,0,0],
 for i in range(len(X)):
    for j in range(len(X[0])):
        result[j][i] = X[i][j]
+X = [[12,7],
+    [4 ,5],
+    [3 ,8]]
+result = [[X[j][i] for j in range(len(X))] for i in range(len(X[0]))]
+
 for r in result:
    print(r)
-
 
