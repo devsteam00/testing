7820f8230eab0fc974a58a65764803ace9b1a6e2
Raghul <133097101+raghul292001@users.noreply.github.com>
2024-12-19 14:46:45 +0530

    Update sort.py
    added vowels

--- sort.py
+++ sort.py
@@ -2,5 +2,13 @@ wrdstr = "Hello this Is an Example With cased letters"
 words = [word.lower() for word in wrdstr.split()]
 words.sort()
 print("The sorted words are:")
-for word in words:
-   print(word)
+vowels = 'aeiou'
+ip_str = 'Hello, have you tried our tutorial section yet?'
+ip_str = ip_str.casefold()
+count = {}.fromkeys(vowels,0)
+for char in ip_str:
+   if char in count:
+       count[char] += 1
+
+print(count)
+
